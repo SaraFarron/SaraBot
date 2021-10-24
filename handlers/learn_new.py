d57bc196be33d2c8f78db8f71ff9117c5f722fcc
Nikita Syulin <nikita_sulin@idpowers.com>
2021-10-24 15:31:00 +0700

    learning in progress

--- handlers/learn_new.py
+++ handlers/learn_new.py
@@ -11,4 +11,16 @@ async def learn_new(message: Message):
     logger.info('Started learning')
 
     get_data('learning', order_by='date_last_learned')
+    await message.answer('Looking for words that you need to learn', reply_markup=ReplyKeyboardRemove())
+
 #     TODO
+
+
+def writing_test(russian: str, english: str):
+    """A test in which user needs to write correct translation"""
+    pass
+
+
+def choose_right_test(russian: str, english: str, wrong_russian: list[str, ], wrong_english: list[str, ]):
+    """A test in which user needs to choose right translation"""
+    pass
